f8894474cd84378ab90ba1646c2ca99d880ecff2
Talal A Gedeon <talalgedeon9@gmail.com>
2021-06-26 15:35:15 +0300

    Edited light logic to test

--- shortproject/src/shortproject.ino
+++ shortproject/src/shortproject.ino
@@ -117,6 +117,10 @@ void loop() {
     leds.setColorRGB(0,0,255,0);
   }
 
+  if (indoorHeatIndex > lowerBound && myVal < outdoorHeatIndex) {
+    leds.setColorRGB(0,0,255,0);
+  }
+
 // Updating OLED Display
   updateDisplay(temp, humidity, tempOutdoor, humidityOutdoor);
 
